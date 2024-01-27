0ac18751398d5ffdbb36609c4984987872823266
Dojah <oladojaabdquadridamilola@gmail.com>
2024-01-27 08:46:48 +0800

    my commit for further error and alignment fixes

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -204,7 +204,7 @@ const Navbar = ({ showsidecard, setShowsideCard, shownavOptions, showDisplayCard
                             </div>
                             <div className="bottomMobiNavSect">
                                 <Link href={`${!currentUser?.admin ? "/profile" : "/dashboard_admin"}`}>Dashboard</Link>
-                                <a href="#about">About</a>
+                                <Link href={"/about"}>About</Link>
                                 <a href="#FAQ">FAQs</a>
                                 <Link href={"/contact"}>Contact</Link>
 
